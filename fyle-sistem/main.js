c194f8f70b43d9d01cf3b14c101d83eff1051e79
fnkbsk <igoico10@gmail.com>
2023-07-25 20:18:17 -0300

    first-commit

--- fyle-sistem/main.js
+++ fyle-sistem/main.js
@@ -1,5 +1,14 @@
 import fs from 'fs';
 
-//Sincronica
+// Sincronica
 
-fs.writeFileSync('./ejemplo.txt', "Hola, Buenas Noches")
+// Escribir txt
+fs.writeFileSync('./ejemplo.txt', "Hola, Buenas Noches");
+
+// Consultar si existe txt
+console.log(fs.existsSync('./ejemplo.txt'))
+
+if(fs.existsSync('./ejemplo.txt')) {
+    //Leer txt
+    let contenido = fs.readFileSync('./ejemplo.txt', 'utf-8');
+}
