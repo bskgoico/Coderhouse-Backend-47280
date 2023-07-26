3ee23f5561b96957b1bc87cb4ebfc723c9b447e5
fnkbsk <igoico10@gmail.com>
2023-07-26 10:44:45 -0300

    first-commit

--- fyle-sistem/main.js
+++ fyle-sistem/main.js
@@ -1,24 +1,32 @@
-import fs from 'fs';
+import fs from "fs";
 
 // Sincronica
 
 // Escribir txt
-fs.writeFileSync('./ejemplo.txt', "Hola, Buenas Noches");
+fs.writeFileSync("./ejemplo.txt", "Hola, Buenas Noches");
 
 // Consultar si existe txt
-console.log(fs.existsSync('./ejemplo.txt'))
+console.log(fs.existsSync("./ejemplo.txt"));
 
-if(fs.existsSync('./ejemplo.txt')) {
-    // Leer txt
-    let contenido = fs.readFileSync('./ejemplo.txt', 'utf-8');
-    console.log(contenido);
+if (fs.existsSync("./ejemplo.txt")) {
+  // Leer txt
+  let contenido = fs.readFileSync("./ejemplo.txt", "utf-8");
+  console.log(contenido);
 
-    // Agregar contenido al txt
-    fs.appendFileSync('./ejemplo.txt', "\n Hola, buenas Noches");
+  // Agregar contenido al txt
+  fs.appendFileSync("./ejemplo.txt", "\n Hola, buenas Noches");
 
-    // Eliminar txt
-    //fs.unlinkSync('./ejemplo.txt');
-};
+  // Eliminar txt
+  //fs.unlinkSync('./ejemplo.txt');
+  //fs.writeFileSync('./ejemplo.txt', "Hola");
+}
 
 // Callbacks
 
+fs.writeFile("./ejemplo.txt", "Hola mundo", (error) => {
+  if (error) return "Error en escritura de archivo";
+  fs.readFile("./ejemplo.txt", "utf-8", (error, resultado) => {
+    if (error) return "Error en lectura de archivo";
+    console.log(error);
+  });
+});
