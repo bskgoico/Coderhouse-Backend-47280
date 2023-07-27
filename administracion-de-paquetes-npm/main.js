c27ff6de439447f54da9c4d549d6e82f5e8d681d
fnkbsk <igoico10@gmail.com>
2023-07-27 20:40:19 -0300

    crypto

--- administracion-de-paquetes-npm/main.js
+++ administracion-de-paquetes-npm/main.js
@@ -35,4 +35,46 @@ http ---> para crear servidores solicitud / respuesta
 path ---> para evitar ambiguedad al trabajar con rutas
 
 
+
+CRYPTO
+
+Proceso de Encriptacion
+
+1) Algoritmo de encriptación
+2) Key o Clave de la encriptación
+3) Iv o Vector inicial (único)    Vector: Una linea recta que esta orientada en algun sentido
+
 */
+
+import * as crypto from 'crypto';
+
+//console.log(crypto.getCiphers()); //Consultar los tipos de algoritmos de encriptacion
+
+const algoritmo = 'aes-256-cbc'
+const key = crypto.randomBytes(32)
+const iv = crypto.randomBytes(16)
+
+//console.log(key);
+//console.log(iv);
+
+//Buffer es como un array para los bytes
+
+
+const encriptar = (password) => {
+    //Poder consultar el Buffer
+    const cipher = crypto.createCipheriv(algoritmo, Buffer.from(key), iv) //Objeto para encriptar la contraseña
+    cipher.update(password) // Preparo el objeto para encriptar
+    let passwordEncriptado = cipher.final() //Resultado de la encriptacion
+    console.log(passwordEncriptado.toString('hex'))
+    return passwordEncriptado
+}
+
+const hackerman = (passwordEncriptada) => {
+    const decipher = crypto.createDecipheriv(algoritmo, Buffer.from(key), Buffer.from(iv, 'hex'))
+    decipher.update(passwordEncriptada)
+    let passwordDes = decipher.final()
+    console.log(passwordDes.toString())
+}
+
+const passwordE = encriptar("Coder1234")
+hackerman(passwordE)
