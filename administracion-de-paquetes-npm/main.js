e9ce9fc04e43fc134bdc2e403b1f448f59c284a2
fnkbsk <igoico10@gmail.com>
2023-07-27 19:24:16 -0300

    first-commit

--- administracion-de-paquetes-npm/main.js
+++ administracion-de-paquetes-npm/main.js
@@ -1,1 +1,22 @@
 // let code = "Le-555-Ar"
+
+/*
+REPASO NODE JS (Corazon de proyectos)
+
+Node js no es solo un modulo mas o una libreria para trabajar.
+Sino que es un entorno de desarrollo completo sobre el cual viven y se ejecutan
+nuestros programas de JavaScript.
+
+
+Surgio de la necesidad de ejectuar Javascript fuera del navegador, y ha crecido hasta convertirse
+en uno de los elementos principales para el desarrollo web.
+
+Cuenta con el mismo motor v8 de Google Chrome, el cual  permite convertir codigo Javascrit
+a código maquina para poder ser procesado correctamente.
+
+Además, cuenta con muchas funcionalidades internas del mismo lenguaje gracias a sus ajustes con ECMAScript.
+
+
+
+
+*/
